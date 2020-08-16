4504d96d61ae25f13d6c90fa4472fac2181ad1ef
skyduy <skyduy@outlook.com>
2020-08-16 17:41:53 +0800

    Complect translate modules.

--- modules/src/tclator.cpp
+++ modules/src/tclator.cpp
@@ -12,6 +12,8 @@
 #include <nlohmann/json.hpp>
 
 using json = nlohmann::json;
+using std::cout;
+using std::endl;
 
 const std::string SPLIT_TOKEN(1, 1);
 
@@ -21,23 +23,40 @@ std::string _calculate(std::string expression) {
     return res;
 }
 
-std::string sha256(const std::string str) {
-    char buf[2];
+httplib::Params getPayload(const std::string& appKey, const std::string& appSecret,
+                           const std::string& q) {
+    auto now = std::chrono::duration_cast<std::chrono::milliseconds>(
+                       std::chrono::system_clock::now().time_since_epoch())
+                       .count();
+    std::string curtime = std::to_string(now / 1000);
+    std::string salt = std::to_string(now);
+
+    std::string truncateQ = q;
+    if (q.length() > 20) {
+        truncateQ = q.substr(0, 10) + std::to_string(q.length()) + q.substr(q.length() - 10, 10);
+    }
+    std::string signStr = appKey + truncateQ + salt + curtime + appSecret;
+
     unsigned char hash[SHA256_DIGEST_LENGTH];
     SHA256_CTX sha256;
     SHA256_Init(&sha256);
-    SHA256_Update(&sha256, str.c_str(), str.size());
+    SHA256_Update(&sha256, signStr.c_str(), signStr.size());
     SHA256_Final(hash, &sha256);
-    std::string NewString = "";
+    std::stringstream ss;
     for (int i = 0; i < SHA256_DIGEST_LENGTH; i++) {
-        sprintf(buf, "%02x", hash[i]);
-        NewString = NewString + buf;
+        ss << std::hex << std::setw(2) << std::setfill('0') << (int)hash[i];
     }
-    return NewString;
+    std::string sign = ss.str();
+    cout << sign << endl;
+    httplib::Params payload{
+            {"from", "auto"}, {"to", "auto"},     {"signType", "v3"}, {"curtime", curtime},
+            {"q", q},         {"appKey", appKey}, {"salt", salt},     {"sign", sign},
+    };
+    return payload;
 }
 
-httplib::Params constructPayload(std::string& src) {
-    // parse appKey, appSecret, content
+std::string _translate(std::string src) {
+    // 解析 appKey, appSecret, content
     size_t pos = 0;
 
     pos = src.find(SPLIT_TOKEN);
@@ -50,50 +69,61 @@ httplib::Params constructPayload(std::string& src) {
 
     std::string q = src;
 
-    // construct payload
-    auto now = std::chrono::duration_cast<std::chrono::milliseconds>(
-                       std::chrono::system_clock::now().time_since_epoch())
-                       .count();
-    std::string curtime = std::to_string(now / 1000);
-    std::string salt = std::to_string(now);
+    // http请求
+    httplib::Client cli("openapi.youdao.com");
+    auto payLoad = getPayload(appKey, appSecret, q);
+    auto res = cli.Post("/api", payLoad);
 
-    std::string truncateQ = q;
-    if (q.length() > 20) {
-        truncateQ = q.substr(0, 10) + std::to_string(q.length()) + q.substr(q.length() - 10, 10);
+    // 错误处理
+    if (res->status != 200) {
+        return "Status Code: " + std::to_string(res->status);
     }
-    std::string signStr = appKey + truncateQ + salt + curtime + appSecret;
-    std::string sign = signStr;
-    httplib::Params payload{
-            {"from", "auto"}, {"to", "auto"},     {"signType", "v3"}, {"curtime", curtime},
-            {"q", q},         {"appKey", appKey}, {"salt", salt},     {"sign", sign},
-    };
-    return payload;
-}
-
-std::string _translate(std::string src) {
-    auto payLoad = constructPayload(src);
-
-    httplib::SSLClient cli("https://www.baidu.com");
-
-    if (auto res = cli.Get("/")) {
-        cli.get_openssl_verify_result();
+    auto o = json::parse(res->body);
+    std::string errorCode = o["errorCode"].get<std::string>();
+    if (errorCode != "0") {
+        switch (std::stoi(errorCode)) {
+        case 108:
+            return "[有道智云] 应用ID不正确（请右击托盘图标设置）";
+        case 202:
+            return "[有道智云] 签名检验失败（请右击托盘图标设置）";
+        default:
+            return "[有道智云] 错误代码 " + errorCode;
+        }
     }
-    // auto res = json::parse(r.text);
-    // switch (res["errorCode"]) {
-    // case "108":
-    //    return "[有道智云] 应用ID不正确（请右击托盘图标设置）";
-    // case "202":
-    //    return "[有道智云] 签名检验失败（请右击托盘图标设置）";
-    // default:
-    //    return "[有道智云] 错误代码" + res["errorCode"];
-    //}
 
-    return "";
-}
+    // 构造答案
+    std::string answer;
+    if (o.contains("translation")) {
+        if (o.contains("basic") && o["basic"].contains("phonetic")) {
+            answer += "[" + o["basic"]["phonetic"].get<std::string>() + "] ";
+        }
+        for (auto& ele : o["translation"]) {
+            answer += ele.get<std::string>();
+            answer += SPLIT_TOKEN;
+        }
+    }
+    if (o.contains("basic") && o["basic"].contains("explains")) {
+        for (auto& ele : o["basic"]["explains"]) {
+            answer += ele.get<std::string>();
+            answer += SPLIT_TOKEN;
+        }
+    }
+    if (o.contains("web")) {
+        for (auto& pair : o["web"]) {
+            if (pair["key"].get<std::string>() == q) {
+                continue;
+            }
+            answer += pair["key"].get<std::string>() + ": ";
+            for (size_t i = 0; i < pair["value"].size(); i++) {
+                if (i != 0) {
+                    answer += " | ";
+                }
+                answer += pair["value"][i].get<std::string>();
+            }
+            answer += SPLIT_TOKEN;
+        }
+    }
 
-int main() {
-    std::string appKey = "1";
-    std::string appSecret = "2";
-    std::cout << _translate(appKey + SPLIT_TOKEN + appSecret + SPLIT_TOKEN + "今天周几")
-              << std::endl;
+    answer.pop_back(); // delete last SPLIT_TOKEN
+    return answer;
 }
