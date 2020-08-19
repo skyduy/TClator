7b4379582805ac30fb427b4f3cc70c763179c2e6
skyduy <skyduy@outlook.com>
2020-08-19 20:25:39 +0800

    Complete calculator module.

--- README.md
+++ README.md
@@ -24,7 +24,3 @@ ShortKey: `ALT+Q`
 ![中文到英文翻译](https://github.com/skyduy/TClator/blob/master/doc/images/zh2en.jpg)
 
 ![English to Chinese](https://github.com/skyduy/TClator/blob/master/doc/images/en2zh.jpg)
-## Thanks
-- https://github.com/kurtcarpenter/stack-based-calculator
-- [Youdao API](https://ai.youdao.com/DOCSIRMA/html/%E8%87%AA%E7%84%B6%E8%AF%AD%E8%A8%80%E7%BF%BB%E8%AF%91/API%E6%96%87%E6%A1%A3/%E6%96%87%E6%9C%AC%E7%BF%BB%E8%AF%91%E6%9C%8D%E5%8A%A1/%E6%96%87%E6%9C%AC%E7%BF%BB%E8%AF%91%E6%9C%8D%E5%8A%A1-API%E6%96%87%E6%A1%A3.html)
-- https://github.com/Wox-launcher/Wox.Plugin.Youdao
--- modules/src/tclator.cpp
+++ modules/src/tclator.cpp
@@ -8,8 +8,11 @@
 #include <openssl/sha.h>
 
 #include <chrono>
+#include <cmath>
 #include <iostream>
 #include <nlohmann/json.hpp>
+#include <stack>
+#include <vector>
 
 using json = nlohmann::json;
 using std::cout;
@@ -17,10 +20,176 @@ using std::endl;
 
 const std::string SPLIT_TOKEN(1, 1);
 
+inline int getPriority(char operate) //栈内优先级
+{
+    switch (operate) {
+    case '+':
+    case '-':
+        return 2;
+    case '*':
+    case '/':
+        return 3;
+    case '^':
+        return 4;
+    case '(':
+    case ')':
+        return 1;
+    default:
+        return 0;
+    }
+}
+
 std::string _calculate(std::string expression) {
-    std::string res = expression;
-    std::reverse(res.begin(), res.end());
-    return res;
+    std::stack<char> operate; // 存放运算符
+    std::string suffix;       // 后缀表达式
+
+    bool lastIsOp = false;
+    size_t cur = 0;
+    while (cur < expression.length()) {
+        if (expression[cur] >= '0' && expression[cur] <= '9') { // 数字
+            suffix.push_back('|');
+            while (expression[cur] >= '0' && expression[cur] <= '9') { // 整数部分
+                suffix.push_back(expression[cur++]);
+            }
+            if (expression[cur] == '.') {
+                suffix.push_back(expression[cur++]);
+                while (expression[cur] >= '0' && expression[cur] <= '9') //小数部分
+                {
+                    suffix.push_back(expression[cur++]);
+                }
+            }
+            suffix.push_back('|');
+            lastIsOp = false;
+        } else if (expression[cur] == '(') { // 左括号 优先级最高，放入栈中1
+            operate.push(expression[cur++]);
+            lastIsOp = true;
+        } else if (expression[cur] ==
+                   ')') { // 右括号，将栈内运算符取出放入输出字符串，直到取出左括号
+            cur++;
+            if (operate.empty()) {
+                return "";
+            } else {
+                while (!operate.empty() && operate.top() != '(') {
+                    suffix.push_back(operate.top());
+                    operate.pop();
+                }
+                if (operate.empty()) {
+                    return "";
+                } else {
+                    operate.pop();
+                }
+            }
+            lastIsOp = true;
+        } else if (expression[cur] == '+' || expression[cur] == '-' || expression[cur] == '*' ||
+                   expression[cur] == '/' || expression[cur] == '^') { // 运算符
+
+            if (expression[cur] == '-') { // 负数
+                if (cur == 0 || lastIsOp) {
+                    suffix.push_back('|');
+                    suffix.push_back('-');
+                    cur++;
+                    while (cur < expression.length() &&
+                           (expression[cur] == ' ' || expression[cur] == '\t' ||
+                            expression[cur] == '\n')) {
+                        cur++;
+                    }
+                    if (expression[cur] >= '0' && expression[cur] <= '9') // 数字
+                    {
+                        while (expression[cur] >= '0' && expression[cur] <= '9') { // 整数部分
+                            suffix.push_back(expression[cur++]);
+                        }
+                        if (expression[cur] == '.') {
+                            suffix.push_back(expression[cur++]);
+                            while (expression[cur] >= '0' && expression[cur] <= '9') //小数部分
+                            {
+                                suffix.push_back(expression[cur++]);
+                            }
+                        }
+                    }
+                    suffix.push_back('|');
+                    continue;
+                }
+            }
+
+            //如果读入一般运算符如+-*/，则放入堆栈，但是放入堆栈之前必须要检查栈顶
+            if (operate.empty()) {
+                operate.push(expression[cur++]);
+            } else {
+                char top = operate.top();                            // 栈顶
+                if (getPriority(top) < getPriority(expression[cur])) // 符号优先级低于栈顶
+                {
+                    operate.push(expression[cur++]); //放入栈顶并指向下一个
+                } else // 否则，将栈顶的运算符放入输出字符串。
+                {
+                    while (getPriority(top) >= getPriority(expression[cur])) {
+                        suffix.push_back(top);
+                        operate.pop();
+                        if (!operate.empty()) {
+                            top = operate.top();
+                        } else {
+                            break;
+                        }
+                    }
+                    operate.push(expression[cur++]); //放入栈顶并指向下一个
+                }
+            }
+            lastIsOp = true;
+        } else if (expression[cur] == ' ' || expression[cur] == '\t' || expression[cur] == '\n') {
+            cur += 1;
+            continue;
+        } else {
+            return "";
+        }
+    }
+    //顺序读完表达式，如果栈中还有操作符，则弹出，并放入输出字符串。
+    while (!operate.empty()) {
+        suffix.push_back(operate.top());
+        operate.pop();
+    }
+
+    // cout << "后缀为：" << suffix << endl;
+
+    std::stack<double> number;
+    cur = 0;
+    std::string current;
+    while (cur < suffix.length()) {
+        if (suffix[cur] == '|') { // 数字
+            cur++;
+            int offset = cur;
+            while (suffix[cur] != '|') {
+                cur++;
+            }
+            current = suffix.substr(offset, cur - offset);
+            number.push(std::stod(current));
+        } else {
+            if (number.size() < 2) {
+                return "";
+            }
+
+            current = suffix.substr(cur, 1);
+            double n2 = number.top();
+            number.pop();
+            double n1 = number.top();
+            number.pop();
+            if (current == "+") {
+                number.push(n1 + n2);
+            } else if (current == "-") {
+                number.push(n1 - n2);
+            } else if (current == "*") {
+                number.push(n1 * n2);
+            } else if (current == "/") {
+                number.push(n1 / n2);
+            } else if (current == "^") {
+                number.push(std::pow(n1, n2));
+            }
+        }
+        cur++;
+    }
+    if (number.size() != 1) {
+        return "";
+    } else {
+        return std::to_string(number.top());
+    }
 }
 
 httplib::Params getPayload(const std::string& appKey, const std::string& appSecret,
